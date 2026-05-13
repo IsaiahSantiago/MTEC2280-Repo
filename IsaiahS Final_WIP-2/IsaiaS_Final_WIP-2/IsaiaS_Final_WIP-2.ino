d187cc250f1685eac51b8ca49cbf88da5029e664
Isaiah.S <143666137+IsaiahSantiago@users.noreply.github.com>
2026-05-13 10:27:26 -0400

    Add files via upload

--- IsaiahS Final_WIP-2/IsaiaS_Final_WIP-2/IsaiaS_Final_WIP-2.ino
+++ IsaiahS Final_WIP-2/IsaiaS_Final_WIP-2/IsaiaS_Final_WIP-2.ino
@@ -14,14 +14,52 @@ Final_WIP-2
 #define buttonPin_3 6 //Turn Right
 #define buttonPin_4 7 //Reset Game Button
 
-void setup() {
-  // put your setup code here, to run once:
+void setup() 
+{
+
+  analogReadResolution(12); //Reading 1 - 4026 bit value for Potentiometer
+
+
+  pinMode(buttonPin_1, INPUT_PULLUP);
+  pinMode(buttonPin_2, INPUT_PULLUP);
+  pinMode(buttonPin_3, INPUT_PULLUP);
+  pinMode(buttonPin_4, INPUT_PULLUP);
+
+  potRead(adcPin_1, OUT);
+
+
+  Serial.begin(115200);
+
+}
+
+void loop() 
+{
+
+  //creating a set of boolean per input for storing current state of each button:
+  bool buttonState = digitalRead(buttonPin_1);  
+  bool buttonState = digitalRead(buttonPin_2);  
+  bool buttonState = digitalRead(buttonPin_3);  
+  bool buttonState = digitalRead(buttonPin_4);  
+
+
+
+
+
+  Serial.println(buttonState);  //print state of buttons
+
 
 
 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
 
-}
+
+
+
+
+
+
+
+
+
+
